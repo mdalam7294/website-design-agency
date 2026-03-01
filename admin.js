5c6af0d70ed240bdddedcbfc650395cb730938a9
mdalam7294 <md2470514@gmail.com>
2026-03-01 21:36:46 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,24 +1,62 @@
-import { auth, db } from "./firebase.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { db } from "./firebase.js";
+import { collection, getDocs, updateDoc, deleteDoc, doc } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-window.login = async function() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+const ADMIN_EMAIL="websitedesignagency1@gmail.com";
+const ADMIN_PASS="Mdalam7294110767@";
 
-  await signInWithEmailAndPassword(auth, email, password);
+window.adminLogin=function(){
 
-  loadOrders();
-};
+const email=document.getElementById("adminEmail").value;
+const pass=document.getElementById("adminPass").value;
 
-async function loadOrders() {
-  const querySnapshot = await getDocs(collection(db, "orders"));
-  let output = "<h3>Orders</h3>";
+if(email===ADMIN_EMAIL && pass===ADMIN_PASS){
+document.getElementById("loginBox").style.display="none";
+document.getElementById("dashboard").style.display="block";
+loadOrders();
+}else{
+alert("Invalid Login");
+}
 
-  querySnapshot.forEach(doc => {
-    const data = doc.data();
-    output += `<p>${data.name} - ${data.plan} - $${data.price}</p>`;
-  });
+}
 
-  document.getElementById("orders").innerHTML = output;
+async function loadOrders(){
+
+const querySnapshot=await getDocs(collection(db,"orders"));
+const container=document.getElementById("orders");
+container.innerHTML="";
+
+querySnapshot.forEach((docSnap)=>{
+
+const data=docSnap.data();
+
+container.innerHTML+=`
+<div class="card">
+<h3>${data.service}</h3>
+<p>Name: ${data.name}</p>
+<p>Email: ${data.email}</p>
+<p>Price: ${data.price}</p>
+<p>Status: ${data.status}</p>
+<button onclick="markComplete('${docSnap.id}')">Mark Complete</button>
+<button onclick="deleteOrder('${docSnap.id}')">Delete</button>
+</div>
+`;
+
+});
+
+}
+
+window.markComplete=async function(id){
+
+await updateDoc(doc(db,"orders",id),{
+status:"Completed"
+});
+
+loadOrders();
+}
+
+window.deleteOrder=async function(id){
+
+await deleteDoc(doc(db,"orders",id));
+loadOrders();
 }
