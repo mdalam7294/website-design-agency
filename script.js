34c8ceea28dbd6f24919c14e14794164456da3a4
mdalam7294 <md2470514@gmail.com>
2026-03-01 23:17:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-let currentCurrency = "INR";
-
-function setCurrency(currency) {
-  currentCurrency = currency;
-  updatePrices();
+function orderNow(service, price){
+let message = `Hello, I want to order ${service} for ${price}. Please guide me.`;
+let whatsappURL = "https://wa.me/916299901015?text=" + encodeURIComponent(message);
+window.open(whatsappURL, "_blank");
 }
-
-function updatePrices() {
-  document.querySelectorAll(".price").forEach(el => {
-    if (currentCurrency === "INR") {
-      el.innerText = "₹" + el.dataset.inr;
-      el.nextElementSibling.style.display = "inline-block";
-      el.nextElementSibling.nextElementSibling.style.display = "none";
-    } else {
-      el.innerText = "$" + el.dataset.usd;
-      el.nextElementSibling.style.display = "none";
-      el.nextElementSibling.nextElementSibling.style.display = "inline-block";
-    }
-  });
-}
-
-function toggleMode() {
-  document.body.classList.toggle("dark");
-}
-
-updatePrices();
